462e5605c55102d7253c6af57d525faaa487451e
Ranjeet Kurmi <ranjeet.cs231167@global.org.in>
2026-09-04 10:20:05 +0530

    add binary search

---
+++ BS.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target) {
+
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    vector<int> arr = {2, 5, 8, 12, 16, 23, 38};
+
+    int target = 23;
+
+    cout << binarySearch(arr, target);
+
+    return 0;
+}
